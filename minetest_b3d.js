6a365de58528d34e872e392651dfcbbdc9e90765
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-23 04:20:45 -0500

    Ok now we're moving at a slight pace

--- minetest_b3d.js
+++ minetest_b3d.js
@@ -1,8 +1,5 @@
 (function() {
   var button;
-  function print(input){}
-  function println(input){}
-  var textBuffer = ""
 })();
 
 Plugin.register("minetest_b3d",{
@@ -14,50 +11,38 @@ Plugin.register("minetest_b3d",{
   version: "0.0.1",
   variant: 'both',
   onload() {
-    print = (input) => {
-      const dir = process.cwd()
-      textBuffer += input
-      Blockbench.writeFile(dir + "/testing.txt",{
-        content: textBuffer
-      })
-    }
-    println = (input) => {
-      const dir = process.cwd()
-      textBuffer += input + "\n"
-      Blockbench.writeFile(dir + "/testing.txt",{
-        content: textBuffer
-      })
-    }
 
-    // Plugin clears all nonsense
-    print("")
+    console.log("yerp")
 
     button = new Action('randomize_height', {
         name: 'Export B3D Model',
         description: 'Export model as B3D for minetest.',
         icon: 'icon-objects',
         click: function() {
-          print("hi")
+          console.log("hi")
 
-            Cube.selected.forEach(cube => {
-                // cube.to[1] = cube.from[0] + Math.floor(Math.random()*8);
-                
-            });
+          let i = 0
+          Cube.selected.forEach(cube => {
+            // cube.to[1] = cube.from[0] + Math.floor(Math.random()*8);
+            console.log(i)
+            console.log(cube)
+            i++
+          });
 
-            // console.log("hi")
-            // Blockbench.showToastMessage("hi", 5)
-            // Blockbench.notification("hi")
+          // console.log("hi")
+          // Blockbench.showToastMessage("hi", 5)
+          // Blockbench.notification("hi")
 
-            
+          
 
 
 
-            // Canvas.updateView({
-            //     elements: Cube.selected,
-            //     element_aspects: {geometry: true},
-            //     selection: true
-            // });
-            // Undo.finishEdit('Export B3D model for minetest.');
+          // Canvas.updateView({
+          //     elements: Cube.selected,
+          //     element_aspects: {geometry: true},
+          //     selection: true
+          // });
+          // Undo.finishEdit('Export B3D model for minetest.');
         }
     });
     MenuBar.addAction(button, 'file.export.0');
